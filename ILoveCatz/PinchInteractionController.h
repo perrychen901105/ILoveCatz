4e938fc2276966b24bbdbad18fa00cb616cf4ba9
perrychen901105 <chenzhipeng901105@gmail.com>
2014-08-05 18:29:48 +0800

    finish first code

---
+++ ILoveCatz/PinchInteractionController.h
@@ -0,0 +1,15 @@
+//
+//  PinchInteractionController.h
+//  ILoveCatz
+//
+//  Created by Perry on 14-8-5.
+//  Copyright (c) 2014年 com.razeware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PinchInteractionController : UIPercentDrivenInteractiveTransition
+
+//- (void)wireToViewController:
+
+@end
---
+++ ILoveCatz/PinchInteractionController.m
@@ -0,0 +1,13 @@
+//
+//  PinchInteractionController.m
+//  ILoveCatz
+//
+//  Created by Perry on 14-8-5.
+//  Copyright (c) 2014年 com.razeware. All rights reserved.
+//
+
+#import "PinchInteractionController.h"
+
+@implementation PinchInteractionController
+
+@end
